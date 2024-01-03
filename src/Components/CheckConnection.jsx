12ea48e21d9ddc64a6b19f61e8deaaad5adfa58c
Aman-Singh123 <amansingh701173@gmail.com>
2024-01-03 16:45:11 +0530

    check connection

--- src/Components/CheckConnection.jsx
+++ src/Components/CheckConnection.jsx
@@ -1,28 +1,34 @@
-import React from "react";
-import { Detector } from 'react-detect-offline'
-import nointernetimg from  '../Images/Png/no-internet.png'
-const CheckConnection = props => {
+import React, { useEffect } from "react";
+import { Detector } from 'react-detect-offline';
+import nointernetimg from '../Images/Png/no-internet.png';
+
+const CheckConnection = (props) => {
+    // Preload the image to ensure it's available offline
+    useEffect(() => {
+        const image = new Image();
+        image.src = nointernetimg;
+    }, []);
+
     return (
         <>
             <Detector
-                render={
-                    ({ online }) => (
-                        online ? props.children :
-                        // You can also use any other component here
-                            <div style={{ paddingTop: '10px', textAlign: 'center' }}>
-                                <img src={ nointernetimg } alt="No Connection Image" />
-                                <h1 style={{ marginBottom: '10px', }}>No Internet Connection </h1>
-                                <h4 style={{margin:'0'}} >Please Check your internet Connection </h4>
+                render={({ online }) => {
+                    console.log('Online status:', online);
 
-                            </div>
-                    )
-                
-                } 
-            
-            
+                    return online ? (
+                        props.children
+                    ) : (
+                        <div style={{ paddingTop: '10px', textAlign: 'center' }}>
+                            <img src={nointernetimg} alt="No Connection Image" style={{ maxWidth: '100%', maxHeight: '100%' }} />
+                            <h1 style={{ marginBottom: '10px' }}>No Internet Connection</h1>
+                            <h4 style={{ margin: '0' }}>Please Check Your Internet Connection</h4>
+                        </div>
+                    );
+                }}
             />
         </>
-    )
-}
+    );
+};
 
-export default CheckConnection
+export default CheckConnection;
+
