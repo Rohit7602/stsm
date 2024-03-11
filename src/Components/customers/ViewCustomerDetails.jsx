b3d3e6e0cd744b6765996afb86800d9ef25c6a37
Aman-Singh123 <amansingh701173@gmail.com>
2024-03-11 11:46:44 +0530

    udpates

--- src/Components/customers/ViewCustomerDetails.jsx
+++ src/Components/customers/ViewCustomerDetails.jsx
@@ -137,7 +137,7 @@ const ViewCustomerDetails = () => {
                     )}
                     <h2 className="fw-400 fs-2sm black mb-0 mt-3  ">Average Order Value</h2>
                     <h2 className="fw-400 fs-xs fade_grey mb-0 mt-1  ">
-                      {AvergaeOrderValue.toFixed(2)}
+                      {isNaN(AvergaeOrderValue) ? 0 : AvergaeOrderValue.toFixed(2)}
                     </h2>
                     <h2 className="fw-400 fs-2sm black mb-0 mt-3 ">Registration</h2>
                     <h2 className="fw-400 fs-xs fade_grey mb-0 mt-1 ">
