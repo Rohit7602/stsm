9c5cc3c0ab675dfdf16c7a4b8e60795fc569345f
ashish <boora8837@gamil.com>
2024-08-01 12:49:25 +0530

    code push

--- src/Components/orders/Orderdetails.jsx
+++ src/Components/orders/Orderdetails.jsx
@@ -86,6 +86,7 @@ export default function NewOrder() {
         setLogs(logsData);
       };
       fetchLogs();
+      
       // console.log("message=====================")
     }
   }, [id, orders, setLogs]);
